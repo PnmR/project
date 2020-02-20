3f28357e850747636fe5846ae9d238ca2a887e73
PnmR <mni.pora@gmail.com>
2020-02-20 13:05:59 +0100

    create a generate password page

--- src/App.js
+++ src/App.js
@@ -5,20 +5,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <title>Secure Password-Generate password</title>
+        <nav>
+          <ul>
+            <li>
+              <a href="" className="current">Generate password</a>
+            </li>
+            <li>
+              <a href="">My account</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <body>
+        <button>Generate password</button>
+      </body>
+      <footer>
+        &copy; 2019 Poonam
+      </footer>
+
     </div>
   );
 }

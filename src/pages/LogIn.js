27f88dc8e36bd83e0d60d506bcb7894d585e1cf7
poonam <mni.pora@gmail.com>
2020-04-13 14:49:51 +0200

    Add login page

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,12 +1,46 @@
 import React from "react";
+import "./LogIn.css";
+import axios from "axios";
 
 class LogIn extends React.Component {
+
+    handleSignIn = () => {
+
+    }
+
+    handleSignUp = () => {
+        
+    }
+
     render() {
         return (
-            <div className="LogIn">
-                <h1>
-                    Log LogIn
-                </h1>
+            <div className="logIn">
+                <div className="flexItem leftSide">
+                    <section className="welcome">
+                        <p className="welcomeTitle">Welcome to Secure password</p>
+                        <p>Very good password</p>
+                        <p>Very good password</p>
+                        <p>Very good password</p>
+                    </section>
+                </div>
+                <div className="flexItem rightSide">
+                    <div className="highlightedPanel">
+                        <form className="signIn">
+                            <p className="smallTitle">Sign In with existing account</p>
+                            <input className="textInput" type="email" name="email" placeholder="Email address" />
+                            <input className="textInput" type="password" name="password" placeholder="Password" />
+                            <input className="submitInput" type="submit" value="Sign In" onClick={this.handleSignIn} />
+
+                        </form>
+                    </div>
+                    <div className="highlightedPanel">
+                        <form className="signUp">
+                            <p className="smallTitle">Wanna sign up?</p>
+                            <input className="textInput" type="email" name="email" placeholder="Email address" />
+                            <input className="submitInput" type="submit" value="Sign Up" onClick={this.handleSignUp} />
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
